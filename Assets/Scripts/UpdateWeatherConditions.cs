1926906117b8efcc913960982dfd12b42d32b4ab
devinallen1 <devinallen1@mail.weber.edu>
2019-12-12 23:13:46 -0700

    Initial puss to git
    Puss to git

---
+++ Assets/Scripts/UpdateWeatherConditions.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DigitalRuby.RainMaker;
+
+public class UpdateWeatherConditions : MonoBehaviour
+{
+    //public bool needWeather;
+    public bool newSearch;
+    public bool hasWeather;
+    public TextMeshProUGUI City;
+    public TextMeshProUGUI Temperature;
+    public TextMeshProUGUI MinMaxTemperature;
+    public TextMeshProUGUI WindSpeed;
+    public GameObject rainParticle;
+    public GameObject clouds;
+    public GameObject rainClouds;
+    public GameObject snowClouds;
+    public GameObject snow;
+    public GameObject sun;
+
+    private WeatherAPI_Script weather;
+    private BaseRainScript rainScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //needWeather = false;
+        newSearch = false;
+        hasWeather = false;
+        weather = WeatherAPI_Script.FindObjectOfType<WeatherAPI_Script>();
+        rainScript = BaseRainScript.FindObjectOfType<BaseRainScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (newSearch && hasWeather)
+        {
+            City.text = weather.city + ", " + weather.country;
+            Temperature.text = CalculateDegreeFehrenheit(weather.temperature).ToString("F0") + "°F";
+            MinMaxTemperature.text = "Min: " + CalculateDegreeFehrenheit(weather.minTemperature).ToString("F0") +
+                                     "°F - Max: " + CalculateDegreeFehrenheit(weather.maxTemperature).ToString("F0") + "°F";
+            WindSpeed.text = "Wind Speed: " + weather.wind.ToString();
+            RainMaker();
+            CloudMaker();
+            SnowMaker();
+            SunMaker();
+            newSearch = false;
+            hasWeather = false;
+        }
+
+    }
+
+    private float CalculateDegreeFehrenheit(float kelvin)
+    {
+        float fehrenheit = (kelvin * 1.8f) - 459.67f;
+        return fehrenheit;
+    }
+
+    void RainMaker()
+    {
+        if (weather.weatherID >= 300 && weather.weatherID <= 321)
+        {
+            rainScript.RainIntensity = .5f;
+            rainClouds.SetActive(true);
+        }
+        else if (weather.weatherID >= 500 && weather.weatherID <= 531)
+        {
+            rainScript.RainIntensity = 1.0f;
+            rainClouds.SetActive(true);
+        }
+        else
+        {
+            rainScript.RainIntensity = 0f;
+            rainClouds.SetActive(false);
+        }
+    }
+
+    void CloudMaker()
+    {
+        if (weather.weatherID >= 801 && weather.weatherID <= 804)
+        {
+            clouds.SetActive(true);
+        }
+        else
+        {
+            clouds.SetActive(false);
+        }
+    }
+
+    void SnowMaker()
+    {
+        if (weather.weatherID >= 600 && weather.weatherID <= 622)
+        {
+            snowClouds.SetActive(true);
+            snow.SetActive(true);
+        }
+        else
+        {
+            snowClouds.SetActive(false);
+            snow.SetActive(false);
+        }
+    }
+
+    void SunMaker()
+    {
+        string tempString = weather.icon;
+        string lastCharacter = tempString.Substring(tempString.Length - 1, 1);
+
+        switch (lastCharacter)
+        {
+            case "d":
+                sun.GetComponent<Light>().enabled = true;
+                sun.transform.rotation = Quaternion.Euler(153f, 0f, 0f);
+                break;
+            case "n":
+                sun.transform.rotation = Quaternion.Euler(190.5f, 0f, 0f);
+                if (weather.weatherID >= 300 && weather.weatherID <= 321 || weather.weatherID >= 500 && weather.weatherID <= 531)
+                {
+                    rainScript.RainIntensity = .5f;
+                    rainClouds.SetActive(true);
+                    sun.GetComponent<Light>().enabled = true;
+                }
+                else
+                {
+                    sun.GetComponent<Light>().enabled = false;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/WeatherAPI_Script.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+using TMPro;
+using DigitalRuby.RainMaker;
+
+public class WeatherAPI_Script : MonoBehaviour
+{
+    public string city;
+    public string country;
+    public string weatherDescription;
+    public int weatherID;
+    public string icon;
+    public float temperature;
+    public float minTemperature;
+    public float maxTemperature;
+    public float rain;
+    public float wind;
+    public float clouds;
+    public string searchingCity;
+    public string searchingCountry;
+    public TMP_InputField input_SearchCity;
+    public TMP_InputField input_SearchCountry;
+    public GameObject rainPartical;
+
+
+    private string apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";// ogden,us&appid=70c538965901887a28a187b206c95c28";
+    private string appiKey = "&appid=70c538965901887a28a187b206c95c28";
+    private string appiKey2 = "&appid=a724efed060b5ce0e20338f6c363409f";
+    private UpdateWeatherConditions updateConditions;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateConditions = UpdateWeatherConditions.FindObjectOfType<UpdateWeatherConditions>();
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Button_Search();
+        }
+    }
+
+    IEnumerator SendRequest()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(apiUrl + searchingCity + "," + searchingCountry + appiKey2);
+        yield return www.SendWebRequest(); ;
+        if (www.error == null || www.error == "")
+        {
+            Debug.Log(www.downloadHandler.text);
+            SetWeatherAtributes(www.downloadHandler.text);
+            //updateConditions.hasWeather = true;
+            //RainMaker();
+            //Debug.Log("Response: " + www.ToString());
+        }
+        else
+        {
+            Debug.Log("Error: " + www.error);
+        }
+    }
+
+
+    void SetWeatherAtributes(string jsonString)
+    {
+        var weatherJson = JSON.Parse(jsonString);
+        city = weatherJson["name"].Value;
+        country = weatherJson["sys"]["country"].Value;
+        weatherDescription = weatherJson["weather"][0]["description"].Value;
+        weatherID = weatherJson["weather"][0]["id"].AsInt;
+        temperature = weatherJson["main"]["temp"].AsFloat;
+        minTemperature = weatherJson["main"]["temp_min"].AsFloat;
+        maxTemperature = weatherJson["main"]["temp_max"].AsFloat;
+        rain = weatherJson["rain"]["3h"].AsFloat;
+        clouds = weatherJson["clouds"]["all"].AsInt;
+        wind = weatherJson["wind"]["speed"].AsFloat;
+        icon = weatherJson["weather"][0]["icon"].Value;
+        updateConditions.hasWeather = true;
+    }
+
+    public void Button_Search()
+    {
+        if (input_SearchCity.text != "" && input_SearchCountry.text != "")
+        {
+            searchingCity = input_SearchCity.text;
+            searchingCountry = input_SearchCountry.text;
+            StartCoroutine(SendRequest()); 
+            updateConditions.newSearch = true;
+            //updateConditions.needWeather = true;
+        }
+    }
+
+    
+
+}
